82c0e848239cb63d2236151bc1355b4aa2f07dab
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-06-24 11:16:54 -0300

    some operations that can be done with select

---
+++ minMaxCount.sql
@@ -0,0 +1,24 @@
+SELECT TOP 100
+    MIN([Id])
+FROM
+    [Categoria]
+
+SELECT TOP 100
+    MAX([Id])
+FROM
+    [Categoria]
+
+SELECT TOP 100
+    COUNT([Id])
+FROM
+    [Categoria]
+
+SELECT TOP 100
+    AVG([Id])
+FROM
+    [Categoria]
+
+SELECT TOP 100
+    SUM([Id])
+FROM
+    [Categoria]
